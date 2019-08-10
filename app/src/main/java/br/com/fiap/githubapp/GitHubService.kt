22f69f23aaa2ef6e55cb372389eb62b0de6511ac
Logon Aluno <logonaluno@local.com>
2019-08-09 22:58:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/githubapp/GitHubService.kt
@@ -0,0 +1,10 @@
+package br.com.fiap.githubapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubService {
+    @GET("/users/{username}")
+    fun buscarUsuario(@Path("username") username: String) : Call<Usuario>
+}
